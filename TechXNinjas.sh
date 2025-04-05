a8606e22fde22b9247fab7a614373dfa363b5b69
TechXNinjas <thetechxninjas@gmail.com>
2025-04-05 10:49:06 +0530

    Update TechXNinjas.sh

--- TechXNinjas.sh
+++ TechXNinjas.sh
@@ -1,68 +1,46 @@
-#!/bin/bash
+BLACK=`tput setaf 0`
+RED=`tput setaf 1`
+GREEN=`tput setaf 2`
+YELLOW=`tput setaf 3`
+BLUE=`tput setaf 4`
+MAGENTA=`tput setaf 5`
+CYAN=`tput setaf 6`
+WHITE=`tput setaf 7`
 
-# Bright Foreground Colors
-BLACK_TEXT=$'\033[0;90m'
-RED_TEXT=$'\033[0;91m'
-GREEN_TEXT=$'\033[0;92m'
-YELLOW_TEXT=$'\033[0;93m'
-BLUE_TEXT=$'\033[0;94m'
-MAGENTA_TEXT=$'\033[0;95m'
-CYAN_TEXT=$'\033[0;96m'
-WHITE_TEXT=$'\033[0;97m'
+BG_BLACK=`tput setab 0`
+BG_RED=`tput setab 1`
+BG_GREEN=`tput setab 2`
+BG_YELLOW=`tput setab 3`
+BG_BLUE=`tput setab 4`
+BG_MAGENTA=`tput setab 5`
+BG_CYAN=`tput setab 6`
+BG_WHITE=`tput setab 7`
 
-NO_COLOR=$'\033[0m'
-RESET_FORMAT=$'\033[0m'
-BOLD_TEXT=$'\033[1m'
-UNDERLINE_TEXT=$'\033[4m'
+BOLD=`tput bold`
+RESET=`tput sgr0`
+#----------------------------------------------------start--------------------------------------------------#
 
-# Displaying start message
-echo
-echo "${CYAN_TEXT}${BOLD_TEXT}╔════════════════════════════════════════════════════════╗${RESET_FORMAT}"
-echo "${CYAN_TEXT}${BOLD_TEXT}                  Starting the process...                   ${RESET_FORMAT}"
-echo "${CYAN_TEXT}${BOLD_TEXT}╚════════════════════════════════════════════════════════╝${RESET_FORMAT}"
-echo 
-
-echo "${YELLOW_TEXT}${BOLD_TEXT}Enter REGION: ${RESET_FORMAT}"
-read REGION
-echo "${BLUE_TEXT}${BOLD_TEXT}You entered: $REGION${RESET_FORMAT}"
+echo "${YELLOW}${BOLD}Starting${RESET}" "${GREEN}${BOLD}Execution${RESET}"
 
 gcloud config set compute/region $REGION
 
-echo
-echo "${GREEN_TEXT}${BOLD_TEXT} ==========================  Creating a Cloud Storage Bucket  ========================== ${RESET_FORMAT}"
-echo
 gsutil mb gs://$DEVSHELL_PROJECT_ID
 
-echo
-echo "${GREEN_TEXT}${BOLD_TEXT} ==========================  Downloading an image (ada.jpg)  ========================== ${RESET_FORMAT}"
-echo
 curl https://upload.wikimedia.org/wikipedia/commons/thumb/a/a4/Ada_Lovelace_portrait.jpg/800px-Ada_Lovelace_portrait.jpg --output ada.jpg
 
-echo
-echo "${GREEN_TEXT}${BOLD_TEXT} ==========================  Renaming the image to kitten.png ========================== ${RESET_FORMAT}"
-echo
 mv ada.jpg kitten.png
 
-echo
-echo "${GREEN_TEXT}${BOLD_TEXT} ==========================  Copying kitten.png to the Bucket ========================== ${RESET_FORMAT}"
-echo
 gsutil cp kitten.png gs://$DEVSHELL_PROJECT_ID
 
-echo
-echo "${GREEN_TEXT}${BOLD_TEXT} ==========================  Copying kitten.png from Bucket to Current Directory  ========================== ${RESET_FORMAT}"
-echo
 gsutil cp -r gs://$DEVSHELL_PROJECT_ID/kitten.png .
 
-echo
-echo "${GREEN_TEXT}${BOLD_TEXT} ==========================  Copying kitten.png to image-folder inside the bucket  ========================== ${RESET_FORMAT}"
-echo
 gsutil cp gs://$DEVSHELL_PROJECT_ID/kitten.png gs://$DEVSHELL_PROJECT_ID/image-folder/
 
-echo
-echo "${GREEN_TEXT}${BOLD_TEXT} ========================== Setting IAM Permissions ========================== ${RESET_FORMAT}"
-echo
 gsutil iam ch allUsers:objectViewer gs://$DEVSHELL_PROJECT_ID
-echo
+
+echo "${RED}${BOLD}Congratulations${RESET}" "${WHITE}${BOLD}for${RESET}" "${GREEN}${BOLD}Completing the Lab !!!${RESET}"
+
+#-----------------------------------------------------end----------------------------------------------------------#
 
 echo "${GREEN_TEXT}${BOLD_TEXT}╔════════════════════════════════════════════════════════╗${RESET_FORMAT}"
 echo "${GREEN_TEXT}${BOLD_TEXT}              Lab Completed Successfully!               ${RESET_FORMAT}"
